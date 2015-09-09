aa14bd6ba1ba789b087dc67492b4412219f5c359
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-09-08 21:04:06 -0700

    hw

--- test/server_test.js
+++ test/server_test.js
@@ -1,29 +1,41 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
 var chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
 require('../server');
 
-describe('our server', function() {
+describe('server test', function() {
   it('should respond to a get request', function(done) {
     chai.request('localhost:3000')
       .get('/greet/kasim')
       .end(function(err, res) {
         expect(res.status).to.eql(200);
-        expect(res.body.msg).to.eql('Hello Kasim');
+        expect(res.body).to.eql('Hello Kasim');
         done();
       });
   });
 
-  it('should greet by name for post requests', function(done) {
+  it('do a post greet', function(done) {
     chai.request('localhost:3000')
       .post('/greet')
       .send({name: 'Kasim'})
       .end(function(err, res) {
-        expect(res.body.msg).to.eql('Hello Kasim');
+        expect(res.body.greet).to.eql('Hello Kasim');
+        done();
+      });
+  });
+
+  it('tell the server time', function(done) {
+    chai.request('localhost:3000')
+      .get('/time')
+      .end(function(err, res) {
+        expect(res.status).to.eql(200);
+        var d = new Date();
+        expect(res.body).to.eql(d.toString());
         done();
       });
   });
